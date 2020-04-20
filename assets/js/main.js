5a140b9e7dd2108cebdabfadae21c4d910ae0889
Alex Wang <alexwangnj@gmail.com>
2020-04-19 23:17:14 -0400

    Add structure to the main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,48 @@
-// test stuff
-sample = {
+const questions = [
+  {
+    notes: [
+      {pitch: 60, startTime: 0.0, endTime: 2.0},
+      {pitch: 61, startTime: 2.0, endTime: 4.0}
+    ],
+    totalTime: 4,
+    correctAnswer: 'm2'
+  },
+  {
   notes: [
-    {pitch: 60, startTime: 0.0, endTime: 2.0},
-    {pitch: 61, startTime: 2.0, endTime: 4.0}
+    {pitch: 63, startTime: 0.0, endTime: 2.0},
+    {pitch: 60, startTime: 2.0, endTime: 4.0}
   ],
   totalTime: 4,
-  code: 'm2'
-};
+  correctAnswer: 'm3'
+  }
+]
 
+let shuffledQuestions, currentQuestion;
+
+const speakerBtn = document.querySelector('#speaker-btn');
+speakerBtn.addEventListener("click", playQuestion, false);
 // player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
 player = new mm.Player();
-function playSample() {
-  player.start(sample);
+
+function playQuestion() {
+  player.start(currentQuestion);
 }
 
-playSample();
+function displayQuestion() {
+  console.log("Display question");
+}
 
-// ma code goes here
-const speakerBtn = document.querySelector('#speaker-btn');
-speakerBtn.addEventListener("click", playSample)
+function nextQuestion() {
+  console.log('what');
+}
+
+function init() {
+  shuffledQuestions = questions;
+  currentQuestion = shuffledQuestions[0]; 
+  playQuestion();
+}
+
+init();
+
+
+

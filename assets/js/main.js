0145a380a885e527140976fc0b3c53fe918b65a9
Alex Wang <alexwangnj@gmail.com>
2020-04-19 21:15:12 -0400

    Add sample

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,17 @@
 // test stuff
-TWINKLE_TWINKLE = {
-    notes: [
-      {pitch: 60, startTime: 0.0, endTime: 0.5},
-      {pitch: 60, startTime: 0.5, endTime: 1.0},
-      {pitch: 67, startTime: 1.0, endTime: 1.5},
-      {pitch: 67, startTime: 1.5, endTime: 2.0},
-      {pitch: 69, startTime: 2.0, endTime: 2.5},
-      {pitch: 69, startTime: 2.5, endTime: 3.0},
-      {pitch: 67, startTime: 3.0, endTime: 4.0},
-      {pitch: 65, startTime: 4.0, endTime: 4.5},
-      {pitch: 65, startTime: 4.5, endTime: 5.0},
-      {pitch: 64, startTime: 5.0, endTime: 5.5},
-      {pitch: 64, startTime: 5.5, endTime: 6.0},
-      {pitch: 62, startTime: 6.0, endTime: 6.5},
-      {pitch: 62, startTime: 6.5, endTime: 7.0},
-      {pitch: 60, startTime: 7.0, endTime: 8.0},
-    ],
-    totalTime: 8
-  };
+sample = {
+  notes: [
+    {pitch: 60, startTime: 0.0, endTime: 2.0},
+    {pitch: 61, startTime: 2.0, endTime: 4.0}
+  ],
+  totalTime: 4,
+  code: 'm2'
+};
 
-player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+// player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+player = new mm.Player();
 function playSample() {
-  player.start(TWINKLE_TWINKLE);
+  player.start(sample);
 }
 
 playSample();

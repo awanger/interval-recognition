5d9ffdb91e021fa02dd278a5a27312ea6ea8d505
Alex Wang <alexwangnj@gmail.com>
2020-04-25 22:50:35 -0400

    Perform a little housecleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,20 @@
-const questions = [
-  {
-    notes: [
-      {pitch: 60, startTime: 0.0, endTime: 2.0},
-      {pitch: 61, startTime: 2.0, endTime: 4.0}
-    ],
-    totalTime: 4,
-    correctAnswer: 'm2'
-  },
-  {
-  notes: [
-    {pitch: 63, startTime: 0.0, endTime: 2.0},
-    {pitch: 60, startTime: 2.0, endTime: 4.0}
-  ],
-  totalTime: 4,
-  correctAnswer: 'm3'
-  }
-]
+player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+// player = new mm.Player();
 
-let shuffledQuestions, currentQuestion;
-let selectedAnswer; // only one answer should be selected at a time
-
+let shuffledQuestions, currentQuestion, selectedAnswer;
 const possibleAnswers = document.querySelectorAll(".btn");
-possibleAnswers.forEach(function(button) {
-  button.addEventListener("click", selectAnswer);
-});
-
 const speakerBtn = document.querySelector('#speaker-btn');
-speakerBtn.addEventListener("click", playQuestion, false);
 const actionBtn = document.querySelector("#action-btn");
 const footer = document.querySelector('footer');
 let footerContainer = footer.firstElementChild;
 let comment = document.querySelector(".comment")
 
-player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
-// player = new mm.Player();
-
 
 function resetState() {
+  // reset selectedAnswer
+  selectedAnswer.classList = selectedAnswer.classList[0]; //remove every class except the first one
+  selectedAnswer = null;
+
   // reset footer
   footer.classList = "";
   comment.innerHTML= '<img class="icon" src="assets/icons/help.svg" alt="">'
@@ -46,10 +24,6 @@ function resetState() {
   actionBtn.classList = "";
   actionBtn.removeEventListener("click", nextQuestion, false);
   actionBtn.removeEventListener("click", tryAgain, false);
-
-  // reset selectedAnswer
-  selectedAnswer.classList = selectedAnswer.classList[0]; //remove every class except the first one
-  selectedAnswer = null;
 
 }
 
@@ -137,7 +111,11 @@ function shuffleQuestions() {
 
 function init() {
   shuffleQuestions();
-  playQuestion();
+  possibleAnswers.forEach(function(button) {
+    button.addEventListener("click", selectAnswer);
+  });
+  speakerBtn.addEventListener("click", playQuestion, false);
+  // playQuestion();
   console.log("initialized");
 }
 
